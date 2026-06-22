8bde6d21e2a0114f85791f2292ea57692e49df58
kottenithin95 <kottenithin95@gmail.com>
2026-06-22 23:12:20 +0530

    Fix Render deployment errors

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 python manage.py collectstatic --noinput 
 python manage.py migrate --noinput 
-gunicorn your project name.wsgi:application --bind 0.0.0.0:$PORT 
+gunicorn your nitin_deploy.wsgi:application --bind 0.0.0.0:$PORT 
