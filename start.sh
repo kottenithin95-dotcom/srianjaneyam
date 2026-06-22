6693b15dba0549927a6b502b40a4c244131e5c24
kottenithin95 <kottenithin95@gmail.com>
2026-06-22 23:31:40 +0530

    Fix Gunicorn and add Pillow

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
-python manage.py collectstatic --noinput 
-python manage.py migrate --noinput 
-gunicorn your nitin_deploy.wsgi:application --bind 0.0.0.0:$PORT 
+#!/bin/bash
+python manage.py collectstatic --noinput
+python manage.py migrate --noinput
+gunicorn nitin_deploy.wsgi:application --bind 0.0.0.0:$PORT
